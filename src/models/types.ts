748bfb5d6c9895abc6b4ea9289efd7c916267783
Ross Hunter <hunter.dev.87@gmail.com>
2019-08-06 23:54:55 -0500

    remove unneeded types from src/models/types

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,43 +1,34 @@
-import {Document} from 'mongoose';
-import {TDiffTypeProps} from '../declarations/utilityTypes';
-import {Request} from 'express';
+import { Document } from "mongoose";
+import { TDiffTypeProps } from "../declarations/utilityTypes";
+import { Request } from "express";
 
 // Type for Message documents retrieved by Mongoose (with Mongoose wrapper)
 export interface IMessage extends Document {
-    text: string,
-    createdAt: string,
-    updatedAt: string,
-    author: string,
-    isAuthorizedRequest: (req: Request) => boolean
-};
-
-export type TLeanMessage = TDiffTypeProps<IMessage, Document>;
-
-// This is the type that the 'author' field of Message documents will have
-// in a response.
-export type TTrimmedAuthor = Pick<IUser, 'photos' | 'displayName' | 'createdAt' | 'updatedAt'>
+    text: string;
+    createdAt: string;
+    updatedAt: string;
+    author: string;
+    isAuthorizedRequest: (req: Request) => boolean;
+}
 
 // Type for User documents retrieved by Mongoose (with Mongoose wrapper)
-export interface IUser extends Document {    
-    displayName: string,
+export interface IUser extends Document {
+    displayName: string;
 
     name: {
-        familyName: string,
-        givenName: string
-    },
+        familyName: string;
+        givenName: string;
+    };
 
-    provider: string,
+    provider: string;
 
-    photos: {[value: string]: string} [],
+    photos: { [value: string]: string }[];
 
-    emails: {[value: string]: string} [],
+    emails: { [value: string]: string }[];
 
-    messages: string [],
-    
-    createdAt: string,
+    messages: string[];
 
-    updatedAt: string
-};
+    createdAt: string;
 
-export type TLeanUser = TDiffTypeProps<IUser, Document>;
-
+    updatedAt: string;
+}
